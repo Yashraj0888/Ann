e7206b2b33e887b8f25762b4545c3650ff9a72c9
Yashraj0888 <bloodymoonknite888@gmail.com>
2024-07-04 19:32:03 +0530

    c

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# if you are deploying to https://Yashraj0888.github.io/Ann
 # git push -f git@github.com:Yashraj0888/Ann.git main:gh-pages
 
 cd -
