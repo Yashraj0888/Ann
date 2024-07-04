a655d2cdff2125f748e197750446b57310d8bb67
Yashraj0888 <bloodymoonknite888@gmail.com>
2024-07-04 19:45:57 +0530

    Name n logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Card from './components/Card'
 import Card1 from './components/Card1'
